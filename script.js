6af0912b848a51423a34f8583f09d81d5777ecf8
harsha-kotla <72369147+harsha-kotla@users.noreply.github.com>
2021-07-30 11:07:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (i=0; i<names.length; i++) {
+  name = names[i]
+  if (name[0] == "J") {
+    console.log("Good Bye " + name)
+  } else {
+    console.log("Hello " + name)
+  }
+}
